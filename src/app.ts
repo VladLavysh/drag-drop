b5f6295bb3d4b8bc7e68998066fb16820c0d12a2
Vlad <vlad@beetobee.ru>
2022-08-14 13:35:58 +0200

    namespaces + separated files

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,3 @@
-//// <reference path='models/drag-drop.ts'/>
-//// <reference path='models/project.ts'/>
-//// <reference path='state/index.ts'/>
-//// <reference path='util/validation.ts'/>
-//// <reference path='decorators/autobind.ts'/>
-
-//// <reference path='components/base-component.ts'/>
-//// <reference path='components/project-item.ts'/>
-
 /// <reference path='components/project-input.ts'/>
 /// <reference path='components/project-list.ts'/>
 
